154cacc099320be7adb942351a9fac7a7e948115
Åke Bjerregaard <ake.bjerregaard@nackademin.se>
2022-10-17 22:29:08 +0200

    Mer filer

---
+++ s05_variabler/types/types.py
@@ -0,0 +1,41 @@
+"""skapa en variabel av typen str med namnet:
+variabel_sträng
+som innehåller värdet:
+'Hej'
+"""
+
+"""skapa en variabel av typen int med namnet:
+variabel_heltal
+som innehåller värdet:
+3
+"""
+
+"""skapa en variabel av typen float med namnet:
+variabel_float
+som innehåller värdet:
+0.5
+"""
+
+"""skapa en variabel av typen float med namnet:
+variabel_float
+som innehåller värdet
+0.5
+"""
+
+"""skapa en variabel av typen bool med namnet:
+variabel_boolean
+som innehåller värdet
+True
+"""
+
+assert variabel_sträng == "Hej"
+assert type(variabel_sträng) == str
+
+assert variabel_heltal == 3
+assert type(variabel_heltal) == int
+
+assert variabel_flyttal == 0.5
+assert type(variabel_flyttal) == float
+
+assert variabel_boolean == True
+assert type(variabel_boolean) == bool
---
+++ s05_variabler/types2/test_types2.py
@@ -0,0 +1,17 @@
+
+import unittest
+from types2 import *
+
+assert variabel_sträng == "Då"
+assert type(variabel_sträng) == str
+
+assert variabel_heltal == -7
+assert type(variabel_heltal) == int
+
+assert variabel_flyttal == -6.23
+assert type(variabel_flyttal) == float
+
+assert variabel_boolean == False
+assert type(variabel_boolean) == bool
+
+
