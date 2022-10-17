80a9c2fb4d7a65bdb51024e3ad96cdc516c4afb7
Åke Bjerregaard <ake.bjerregaard@nackademin.se>
2022-10-17 22:44:11 +0200

    Kommentera ut den här

--- s70_felhantering/s60_input/test_error.py
+++ s70_felhantering/s60_input/test_error.py
@@ -1,14 +1,14 @@
-import unittest
+# import unittest
 
-def broken_function():
-    raise Exception('This is broken')
+# def broken_function():
+#     raise Exception('This is broken')
 
-class MyTestCase(unittest.TestCase):
-    def test(self):
-        with self.assertRaises(Exception) as context:
-            broken_function()
+# class MyTestCase(unittest.TestCase):
+#     def test(self):
+#         with self.assertRaises(Exception) as context:
+#             broken_function()
 
-        self.assertTrue('This is broken' in context.exception)
+#         self.assertTrue('This is broken' in context.exception)
 
 
-MyTestCase().test()
+# MyTestCase().test()
