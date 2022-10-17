0f005e57afa1bb7bba10860bbc5bc8feb791ec61
Åke Bjerregaard <ake.bjerregaard@nackademin.se>
2022-10-17 12:55:49 +0200

    arv med fiskar

---
+++ s80_klasser/s70_arv/åke/arv.py
@@ -0,0 +1,15 @@
+class Fisk:
+    
+    def __init__(self, simhastighet):
+        self.simhastighet = simhastighet
+
+class Gädda(Fisk):
+
+    def __init__(self, simhastighet):
+        super().__init__(simhastighet)
+
+
+class Aborre(Fisk):
+    
+    def __init__(self, simhastighet):
+        super().__init__(simhastighet)
