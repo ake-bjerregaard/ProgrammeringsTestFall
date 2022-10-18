e5aef766275d1cf02b8e4d7255dfaf8f7243b26d
Åke Bjerregaard <ake.bjerregaard@nackademin.se>
2022-10-19 01:15:14 +0200

    Höns och kukeliku

---
+++ s70_felhantering/egen_exception_class/egen_exception.py
@@ -0,0 +1,11 @@
+""" Skapa en egen exception class som heter:
+EgenException
+Klassen ska ärva utav 
+Exception  """
+
+
+
+""" Skapa en funktion som heter:
+Kasta
+Metoden ska kasta/raise EgenException
+"""
