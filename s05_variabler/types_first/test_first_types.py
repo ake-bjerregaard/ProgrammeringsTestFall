ed0e3d24c875c07ee837b70f14637cb1fc298e05
Åke Bjerregaard <ake.bjerregaard@nackademin.se>
2022-10-18 22:58:26 +0200

    test_first_types

---
+++ s05_variabler/types_first/test_first_types.py
@@ -0,0 +1,17 @@
+
+import unittest
+from types import *
+
+assert variabel_sträng == "Hej"
+assert type(variabel_sträng) == str
+
+assert variabel_heltal == 3
+assert type(variabel_heltal) == int
+
+assert variabel_flyttal == 0.5
+assert type(variabel_flyttal) == float
+
+assert variabel_boolean == True
+assert type(variabel_boolean) == bool
+
+
